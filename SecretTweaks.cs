633470bfd175b0322ef519d6505a66537942b216
Cara <git@caraxian.com>
2021-11-06 22:49:10 +1030

    Initial commit

---
+++ SecretTweaks.cs
@@ -0,0 +1,13 @@
+﻿using SimpleTweaksPlugin.TweakSystem;
+
+namespace SecretTweaks {
+    public class SecretTweaks : SubTweakManager<SecretTweaks.SubTweak> {
+        public abstract class SubTweak : BaseTweak {
+            public override string Key => $"{nameof(SecretTweaks)}@{base.Key}";
+        }
+
+        public override bool AlwaysEnabled => true;
+
+        public override string Name => "Secret Blah";
+    }
+}
